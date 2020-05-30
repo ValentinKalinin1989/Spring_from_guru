01c3771ec175bf7c8f383d9af00ed0e34f03392d
Valentin Kalinin <k-valentin-1989@yandex.ru>
2020-05-30 19:02:02 +0300

    Enable h2-console.

--- src/main/java/guru/srpingframework/webapp/domain/Publisher.java
+++ src/main/java/guru/srpingframework/webapp/domain/Publisher.java
@@ -1,10 +1,9 @@
 package guru.srpingframework.webapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -19,6 +18,10 @@ public class Publisher {
     private String state;
     private String zip;
 
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
+
     public Publisher() {
     }
 
@@ -32,6 +35,14 @@ public class Publisher {
                 ", state='" + state + '\'' +
                 ", zip='" + zip + '\'' +
                 '}';
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     public Long getId() {
