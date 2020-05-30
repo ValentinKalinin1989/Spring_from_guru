a1fac7cb7bedf23125e5fc5942b7ff9b4b5bdfbf
Valentin Kalinin <k-valentin-1989@yandex.ru>
2020-05-30 21:52:10 +0300

    Add controller and thymeleaf template for authors.

--- src/main/java/guru/srpingframework/webapp/domain/Book.java
+++ src/main/java/guru/srpingframework/webapp/domain/Book.java
@@ -77,8 +77,7 @@ public class Book {
         return "Book{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
-                ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
+                ", isbn='" + isbn +
                 '}';
     }
 
